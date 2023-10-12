6c5d0cefd88f96a23fa8260c09b5ed53d27e1b23
Meiliatan <111167289+Meiliatan@users.noreply.github.com>
2023-10-13 01:54:02 +0700

    Delete Day 2/kyc directory

--- Day 2/kyc/src/main/java/com/multipolar/bootcamp/kyc/repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.multipolar.bootcamp.kyc.repository;
-
-import com.multipolar.bootcamp.kyc.domain.Customer;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CustomerRepository extends MongoRepository<Customer, String> {
-}
